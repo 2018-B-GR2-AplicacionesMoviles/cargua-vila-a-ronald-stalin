985b689f85e40726fccb4cc2665c5ef5bb6843d9
Ronald Cargua <ronald_96c@hotmail.com>
2018-11-15 13:32:54 -0500

    deber

---
+++ 02-Android/app/src/main/java/com/example/ronald/a2018movilesgr2/Button.kt
@@ -0,0 +1,13 @@
+package com.example.ronald.a2018movilesgr2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Button : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_button)
+
+    }
+}
---
+++ Deber/app/src/main/java/com/example/ronald/deber/Calendario.kt
@@ -0,0 +1,12 @@
+package com.example.ronald.deber
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Calendario : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendario)
+    }
+}
